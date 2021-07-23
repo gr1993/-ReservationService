89ce85eb950234d0d0232bc7debd78c190884e13
ParkKanglim <pgr@kipis.co.kr>
2021-07-23 15:31:46 +0900

    [프론트] 전체 컨텐츠 미디어 쿼리 작업

--- reserve-front/src/App.tsx
+++ reserve-front/src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Navigator from './components/navigator';
 import MainPage from './pages/mainPage';
 import EmptyPage from './pages/emptyPage';
 
+const StyledDiv = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  @media only screen and (min-width: 1024px) {
+    margin:0px auto;
+  }
+`;
+
 function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navigator />
-        <Switch>
-          <Route exact path="/">
-            <MainPage />
-          </Route>
-          <Route>
-            <EmptyPage />
-          </Route>
-        </Switch>
-      </div>
+      <StyledDiv>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Navigator />
+          </Grid>
+          <Grid item xs={12}>
+            <Switch>
+              <Route exact path="/">
+                <MainPage />
+              </Route>
+              <Route>
+                <EmptyPage />
+              </Route>
+            </Switch>
+          </Grid>
+        </Grid>
+      </StyledDiv>
     </BrowserRouter>
   );
 }
--- reserve-front/src/pages/mainPage.tsx
+++ reserve-front/src/pages/mainPage.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  width: 100%;
+  height: 500px;
+  background-image:url('img/plane-4301615_1280.png');
+  background-size:cover;
+  background-repeat:no-repeat;
+`;
 
 function MainPage(): JSX.Element {
   return (
-    <div>메인</div>
+    <StyledDiv />
   );
 }
 
