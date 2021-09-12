b2c6162436ba796cb111d8d0e48888cca3858cde
DESKTOP-889J8FU\park <test@test.com>
2021-09-12 10:34:50 +0900

    [백엔드] validator 미들웨어 추가

--- reserve-back/src/member/dto/create-member.dto.ts
+++ reserve-back/src/member/dto/create-member.dto.ts
@@ -1,6 +1,15 @@
+import { IsString } from 'class-validator';
+
 export class CreateMemberDto {
+  @IsString()
   id: string;
+
+  @IsString()
   password: string;
+
+  @IsString()
   name: string;
+
+  @IsString()
   mobile: string;
 }
