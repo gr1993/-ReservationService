8ae0c4831ba69dd5fdefcfcb43dda0febdff5911
DESKTOP-889J8FU\park <test@test.com>
2021-09-14 08:18:28 +0900

    [백엔드] 모듈에서 Config 정보 불러오도록 구현

--- reserve-back/src/auth/auth.module.ts
+++ reserve-back/src/auth/auth.module.ts
@@ -3,12 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { MemberModule } from '../member/member.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: 'TEST_KEY',
-      signOptions: { expiresIn: '60m' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: '60m' },
+      }),
+      inject: [ConfigService],
     }),
     MemberModule,
   ],
