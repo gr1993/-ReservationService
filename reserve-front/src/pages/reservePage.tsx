0e1001b32e7792841ffde725286bc995075d3e83
DESKTOP-889J8FU\park <test@test.com>
2021-09-15 19:14:57 +0900

    [프론트] 예약페이지 디자인 초안 완성

--- reserve-front/src/pages/reservePage.tsx
+++ reserve-front/src/pages/reservePage.tsx
@@ -7,6 +7,7 @@ import {
   Select,
   TextField,
 } from '@material-ui/core';
+import { DataGrid } from '@material-ui/data-grid';
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
@@ -16,7 +17,7 @@ import { RootReducerType } from '../state/store';
 const StyledMainDiv = style.div`
   margin: 0px auto;
   width: 100%;
-  height: 800px;
+  height: 620px;
   border: 1px solid rgba(190, 190, 190, .5);
   border-radius: 5px;
 `;
@@ -70,6 +71,48 @@ const CustomField: React.FC<CustomFieldProps> = ({ labelText }): JSX.Element => 
     </Grid>
   );
 };
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
 const ReservePage = (): JSX.Element => {
   const history = useHistory();
@@ -140,6 +183,23 @@ const ReservePage = (): JSX.Element => {
             </Button>
           </Grid>
         </Grid>
+        <div style={{ marginTop: '15px', height: 400, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            checkboxSelection
+            disableSelectionOnClick
+          />
+        </div>
+        <Button
+          style={{ margin: '10px 15px 0px 0px', float: 'right', width: '150px', height: '60px' }}
+          className="ButtonStyle"
+          variant="contained"
+          color="secondary"
+        >
+          예약하기
+        </Button>
       </div>
     </StyledMainDiv>
   );
