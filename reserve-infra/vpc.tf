7a85083a84ab74615f709fd7ef820d4109bf1a21
DESKTOP-889J8FU\park <test@test.com>
2021-09-21 15:55:07 +0900

    [인프라] security group 룰 몇 개 추가

--- reserve-infra/vpc.tf
+++ reserve-infra/vpc.tf
@@ -7,11 +7,29 @@ resource "aws_security_group" "allow_http" {
   vpc_id      = aws_default_vpc.default.id
 }
 
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_http.id
+}
+
 resource "aws_security_group_rule" "http" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_http.id
+}
+
+resource "aws_security_group_rule" "internet" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.allow_http.id
 }
